a879166ef0310d4e69fd23f4ce6a732b1cf8b1fd
andy <andy.augustin@t-online.de>
2014-11-01 15:58:21 +0100

    added project added logger adapter for NLog and contracts

---
+++ src/DocToMarkdown.Common/Adapter/NLogManagerAdapter.cs
@@ -0,0 +1,48 @@
+﻿//  *************************************************************
+// <copyright file="NLogManagerAdapter.cs" company="None">
+//     Copyright (c) 2014 andy.  All rights reserved.
+// </copyright>
+// <license>MIT Licence</license>
+// <author>andy</author>
+// <email>andy.augustin@t-online.de</email>
+// *************************************************************
+
+namespace DocToMarkdown.Common
+{
+    using System;
+    using System.Collections.Generic;
+
+    using NLog;
+
+    /// <summary>
+    /// Adapter for the <see cref="LogManager"/>
+    /// </summary>
+    public sealed class NLogManagerAdapter : ILoggerManager
+    {
+        #region fields
+
+        private Dictionary<String, ILogger> _loggerPool = new Dictionary<String, ILogger>();
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Gets the specified logger.
+        /// </summary>
+        /// <returns>The logger.</returns>
+        /// <param name="loggerName">The logger name.</param>
+        public ILogger GetLogger(String loggerName)
+        {
+            if (!this._loggerPool.ContainsKey(loggerName))
+            {
+                var logger = LogManager.GetLogger(loggerName);
+                this._loggerPool.Add(loggerName, new NLogAdapter(logger));
+            }
+
+            return this._loggerPool[loggerName];
+        }
+
+        #endregion
+    }
+}
---
+++ src/DocToMarkdown.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿//  *************************************************************
+// <copyright file="AssemblyInfo.cs" company="None">
+//     Copyright (c) 2014 andy.  All rights reserved.
+// </copyright>
+// <license>MIT Licence</license>
+// <author>andy</author>
+// <email>andy.augustin@t-online.de</email>
+// *************************************************************
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+[assembly: AssemblyTitle("DocToMarkdown.Common")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("andy")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// Make internals visible for tests
+[assembly: InternalsVisibleTo("DocToMarkdown.Common.Tests")]

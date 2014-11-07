0dca9616422bb339c8901fb68911c8cf3b7d2cad
andy <andy.augustin@t-online.de>
2014-11-07 19:27:34 +0100

    added test

---
+++ src/Tests/DocToMarkdown.Tests/MarkdownNodeParser/ExceptionMarkdownNodeParserTest.cs
@@ -0,0 +1,107 @@
+﻿//  *************************************************************
+// <copyright file="ExceptionMarkdownNodeParserTest.cs" company="None">
+//     Copyright (c) 2014 andy.  All rights reserved.
+// </copyright>
+// <license>MIT Licence</license>
+// <author>andy</author>
+// <email>andy.augustin@t-online.de</email>
+// *************************************************************
+
+namespace DocToMarkdown.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml.Linq;
+
+    using DocToMarkdown.Common;
+
+    using NSubstitute;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test for the <see cref="ExampleMarkdownNodeParser"/> class.
+    /// </summary>
+    [TestFixture]
+    public class ExceptionMarkdownNodeParserTest
+    {
+        #region fields
+
+        private const String NodeString = "<exception cref=\"System.Exception\">Thrown when...</exception>";
+
+        #endregion
+
+        #region properties
+
+        private XElement XmlInput
+        {
+            get
+            {
+                return XElement.Parse(NodeString);
+            }
+        }
+
+        private IEnumerable<TestCaseData> TestDataSource
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    MarkdownType.Markdown,
+                    String.Format(
+                        ">**Exception:** [{0}](#{0}):{2}> {1}{2}{2}",
+                        "System.Exception",
+                        "Thrown when...",
+                        Environment.NewLine));
+                yield return new TestCaseData(
+                    MarkdownType.GithubFlavoredMarkdown,
+                    String.Format(
+                        ">**Exception:** *{0}*:{2}> {1}{2}{2}",
+                        "System.Exception",
+                        "Thrown when...",
+                        Environment.NewLine));
+            }
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Test for creating a new object.
+        /// </summary>
+        [Test]
+        [Category("Unit test: parser")]
+        [TestCase(MarkdownType.GithubFlavoredMarkdown)]
+        [TestCase(MarkdownType.Markdown)]
+        public void Init_CreateObject_IsNotNull(Int32 markdownType)
+        {
+            var parserPoolStub = Substitute.For<IParserPool>();
+            var environmentStub = Substitute.For<IEnvironment>();
+
+            var obj = new ExceptionMarkdownNodeParser(parserPoolStub, environmentStub, (MarkdownType)markdownType);
+
+            Assert.IsNotNull(obj);
+        }
+
+        /// <summary>
+        /// Parses the pars input element result equals expected.
+        /// </summary>
+        [Test]
+        [Category("Unit test: parser")]
+        [TestCaseSource("TestDataSource")]
+        public void Parse_ParseInputElement_ResultEqualsExpected(Int32 markdownType, String expected)
+        {
+            var input = this.XmlInput;
+            var parserPoolStub = Substitute.For<IParserPool>();
+            var environmentMock = Substitute.For<IEnvironment>();
+            environmentMock.NewLine.Returns(Environment.NewLine);
+
+            var parser = new ExceptionMarkdownNodeParser(parserPoolStub, environmentMock, (MarkdownType)markdownType);
+
+            var result = parser.ParseToMarkdown(input);
+
+            StringAssert.AreEqualIgnoringCase(expected, result);
+        }
+
+        #endregion
+    }
+}

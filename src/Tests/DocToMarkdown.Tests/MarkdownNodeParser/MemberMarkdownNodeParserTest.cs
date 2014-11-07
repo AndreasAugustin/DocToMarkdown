c8b04436359903cf3e168aaa0e862e2b10252526
andy <andy.augustin@t-online.de>
2014-11-07 19:35:18 +0100

    added test, but is ignored for now.

---
+++ src/Tests/DocToMarkdown.Tests/MarkdownNodeParser/MemberMarkdownNodeParserTest.cs
@@ -0,0 +1,95 @@
+﻿//  *************************************************************
+// <copyright file="MemberMarkdownNodeParserTest.cs" company="None">
+//     Copyright (c) 2014 andy.  All rights reserved.
+// </copyright>
+// <license>MIT Licence</license>
+// <author>andy</author>
+// <email>andy.augustin@t-online.de</email>
+// *************************************************************
+
+namespace DocToMarkdown.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml.Linq;
+
+    using DocToMarkdown.Common;
+
+    using NSubstitute;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test for the <see cref="ExampleMarkdownNodeParser"/> class.
+    /// </summary>
+    [TestFixture]
+    public class MemberMarkdownNodeParserTest
+    {
+        #region fields
+
+        private const String NodeString = "<member name=\"T:DocToMarkdown.Program\">" +
+                                          "<summary>Starts the console application. Some input for this project came from " +
+                                          "<see href=\"https://gist.github.com/lontivero/593fc51f1208555112e0\"/>" +
+                                          "</summary>" +
+                                          "</member>";
+
+        #endregion
+
+        #region properties
+
+        private XElement XmlInput
+        {
+            get
+            {
+                return XElement.Parse(NodeString);
+            }
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Test for creating a new object.
+        /// </summary>
+        [Test]
+        [Category("Unit test: parser")]
+        [TestCase(MarkdownType.GithubFlavoredMarkdown)]
+        public void Init_CreateObject_IsNotNull()
+        {
+            var parserPoolStub = Substitute.For<IParserPool>();
+            var environmentStub = Substitute.For<IEnvironment>();
+
+            var obj = new MemberMarkdownNodeParser(parserPoolStub, environmentStub);
+
+            Assert.IsNotNull(obj);
+        }
+
+        /// <summary>
+        /// Parses the pars input element result equals expected.
+        /// </summary>
+        [Test]
+        [Category("Unit test: parser")]
+        [Ignore]
+        public void Parse_ParseInputElement_ResultEqualsExpected()
+        {
+            var input = this.XmlInput;
+            var parserPoolStub = Substitute.For<IParserPool>();
+            var environmentMock = Substitute.For<IEnvironment>();
+            environmentMock.NewLine.Returns(Environment.NewLine);
+
+            var parser = new MemberMarkdownNodeParser(parserPoolStub, environmentMock);
+
+            var result = parser.ParseToMarkdown(input);
+
+            var expected = String.Format(
+                               "---{2}### Type: {0}{2}{2}{1}{2}{2}",
+                               "DocToMarkdown.Program",
+                               "{1}",
+                               environmentMock.NewLine);
+
+            StringAssert.AreEqualIgnoringCase(expected, result);
+        }
+
+        #endregion
+    }
+}

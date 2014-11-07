1de0db9790a59bd9b2dabb2d4513223f0e1f0929
andy <andy.augustin@t-online.de>
2014-11-07 19:14:30 +0100

    added test

---
+++ src/Tests/DocToMarkdown.Tests/MarkdownNodeParser/ExampleMarkdownNodeParserTest.cs
@@ -0,0 +1,87 @@
+﻿//  *************************************************************
+// <copyright file="ExampleMarkdownNodeParserTest.cs" company="None">
+//     Copyright (c) 2014 andy.  All rights reserved.
+// </copyright>
+// <license>MIT Licence</license>
+// <author>andy</author>
+// <email>andy.augustin@t-online.de</email>
+// *************************************************************
+
+namespace DocToMarkdown.Tests
+{
+    using System;
+    using System.Xml.Linq;
+
+    using DocToMarkdown.Common;
+
+    using NSubstitute;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test for the <see cref="ExampleMarkdownNodeParser"/> class.
+    /// </summary>
+    [TestFixture]
+    public class ExampleMarkdownNodeParserTest
+    {
+        #region fields
+
+        private const String NodeString = "<example>This sample shows how to call the GetZero method.</example>";
+
+        #endregion
+
+        #region properties
+
+        private XElement XmlInput
+        {
+            get
+            {
+                return XElement.Parse(NodeString);
+            }
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Test for creating a new object.
+        /// </summary>
+        [Test]
+        [Category("Unit test: parser")]
+        public void Init_CreateObject_IsNotNull()
+        {
+            var parserPoolStub = Substitute.For<IParserPool>();
+            var environmentStub = Substitute.For<IEnvironment>();
+
+            var obj = new ExampleMarkdownNodeParser(parserPoolStub, environmentStub);
+
+            Assert.IsNotNull(obj);
+        }
+
+        /// <summary>
+        /// Parses the pars input element result equals expected.
+        /// </summary>
+        [Test]
+        [Category("Unit test: parser")]
+        public void Parse_ParseInputElement_ResultEqualsExpected()
+        {
+            var input = this.XmlInput;
+            var parserPoolStub = Substitute.For<IParserPool>();
+            var environmentMock = Substitute.For<IEnvironment>();
+            environmentMock.NewLine.Returns(Environment.NewLine);
+
+            var parser = new ExampleMarkdownNodeParser(parserPoolStub, environmentMock);
+
+            var result = parser.ParseToMarkdown(input);
+
+            var expected = String.Format(
+                               "{1}**Example:**<big><pre>{0}{1}</pre></big>{1}",
+                               "This sample shows how to call the GetZero method.",
+                               environmentMock.NewLine);
+
+            StringAssert.AreEqualIgnoringCase(expected, result);
+        }
+
+        #endregion
+    }
+}

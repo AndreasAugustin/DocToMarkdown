5dac73d7f284a59a8e55f675bc18fc9ca95ee86f
andy <andy.augustin@t-online.de>
2014-11-07 19:39:22 +0100

    added test

---
+++ src/Tests/DocToMarkdown.Tests/MarkdownNodeParser/ParamMarkdownNodeParserTest.cs
@@ -0,0 +1,88 @@
+﻿//  *************************************************************
+// <copyright file="ParamMarkdownNodeParserTest.cs" company="None">
+//     Copyright (c) 2014 andy.  All rights reserved.
+// </copyright>
+// <license>MIT Licence</license>
+// <author>andy</author>
+// <email>andy.augustin@t-online.de</email>
+// *************************************************************
+
+namespace DocToMarkdown.Tests
+{
+    using System;
+    using System.Xml.Linq;
+
+    using DocToMarkdown.Common;
+
+    using NSubstitute;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test for the <see cref="CodeMarkdownNodeParser"/> class.
+    /// </summary>
+    [TestFixture]
+    public class ParamMarkdownNodeParserTest
+    {
+        #region fields
+
+        private const String NodeString = "<param name=\"Int1\">Used to indicate status.</param>";
+
+        #endregion
+
+        #region properties
+
+        private XElement XmlInput
+        {
+            get
+            {
+                return XElement.Parse(NodeString);
+            }
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Test for creating a new object.
+        /// </summary>
+        [Test]
+        [Category("Unit test: parser")]
+        public void Init_CreateObject_IsNotNull()
+        {
+            var parserPoolStub = Substitute.For<IParserPool>();
+            var environmentStub = Substitute.For<IEnvironment>();
+
+            var obj = new ParamMarkdownNodeParser(parserPoolStub, environmentStub);
+
+            Assert.IsNotNull(obj);
+        }
+
+        /// <summary>
+        /// Parses the pars input element result equals expected.
+        /// </summary>
+        [Test]
+        [Category("Unit test: parser")]
+        public void Parse_ParseInputElement_ResultEqualsExpected()
+        {
+            var input = this.XmlInput;
+            var parserPoolStub = Substitute.For<IParserPool>();
+            var environmentMock = Substitute.For<IEnvironment>();
+            environmentMock.NewLine.Returns(Environment.NewLine);
+
+            var parser = new ParamMarkdownNodeParser(parserPoolStub, environmentMock);
+
+            var result = parser.ParseToMarkdown(input);
+
+            var expected = String.Format(
+                               "{2}> **Parameter** *{0}*: {1} {2}",
+                               "Int1",
+                               "Used to indicate status.",
+                               environmentMock.NewLine);
+
+            StringAssert.AreEqualIgnoringCase(expected, result);
+        }
+
+        #endregion
+    }
+}

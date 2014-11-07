08358dcab30cbf2a99d924df95cde15783c33555
andy <andy.augustin@t-online.de>
2014-11-07 22:45:47 +0100

    Changed configurationadapter: Now the config file should be loaded while application starts, not only during compilation

--- src/DocToMarkdown.Common/Adapter/ConfigurationAdapter.cs
+++ src/DocToMarkdown.Common/Adapter/ConfigurationAdapter.cs
@@ -10,16 +10,23 @@
 namespace DocToMarkdown.Common
 {
     using System;
-    using System.Configuration;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Linq;
 
     /// <summary>
-    /// Adapter for the <see cref="ConfigurationManager"/> class to get the application data.
+    /// Adapter for getting values from the App.config file.
     /// </summary>
     public class ConfigurationAdapter : IConfiguration
     {
         #region fields
 
+        private String _baseDirectory;
+        private const String configFileName = "App.config";
         private ILogger _logger;
+        private XDocument _configDocument;
+        private XElement _configurationElement;
+        private XElement _appSettingsElement;
 
         #endregion
 
@@ -29,9 +36,60 @@ namespace DocToMarkdown.Common
         /// Initializes a new instance of the <see cref="DocToMarkdown.Common.ConfigurationAdapter"/> class.
         /// </summary>
         /// <param name="loggerManager">Logger manager.</param>
-        public ConfigurationAdapter(ILoggerManager loggerManager)
+        /// <param name = "baseDirectory"></param>
+        public ConfigurationAdapter(ILoggerManager loggerManager, String baseDirectory)
         {
             this._logger = loggerManager.GetLogger("Configuration");
+            this._baseDirectory = baseDirectory;
+        }
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Gets or sets the config document.
+        /// Made internal for dependency injection out of testing purposes.
+        /// </summary>
+        /// <value>The config document.</value>
+        internal XDocument ConfigDocument
+        {
+            get
+            {
+                return _configDocument ?? (this._configDocument = XDocument.Load(Path.Combine(
+                        this._baseDirectory,
+                        configFileName)));
+            }
+
+            set
+            {
+                _configDocument = value;
+            }
+        }
+
+        private XElement ConfigurationElement
+        {
+            get
+            {
+                return this._configurationElement ?? (this._configurationElement = this.ConfigDocument.Element("configuration"));
+            }
+        }
+
+        private XElement AppSettingsElement
+        {
+            get
+            {
+                if (this._appSettingsElement != null)
+                {
+                    return this._appSettingsElement;
+                }
+
+                this._appSettingsElement = this.ConfigurationElement.Element("appSettings");
+
+                this.CheckDistinctAppSettingKeys();
+
+                return this._appSettingsElement;
+            }
         }
 
         #endregion
@@ -49,8 +107,14 @@ namespace DocToMarkdown.Common
         {
             get
             {
-                var value = ConfigurationManager.AppSettings[key];
+                var element = this.AppSettingsElement.Elements().FirstOrDefault(e => e.Attribute("key").Value == key);
 
+                if (element == null)
+                {
+                    var errorMessage = String.Format("The key {0} was not found in the config file", key);
+                }
+
+                var value = element.Attribute("value").Value;
                 if (String.IsNullOrEmpty(value))
                 {
                     var errorMessage = String.Format(
@@ -59,13 +123,31 @@ namespace DocToMarkdown.Common
 
                     this._logger.Error(String.Format(errorMessage));
 
-                    throw new ConfigurationErrorsException(errorMessage);
+                    throw new ApplicationException(errorMessage);
                 }
 
                 this._logger.Info(String.Format("Configuration for key: {0} valid with value: {1}", key, value));
 
                 return value;
             }
+        }
+
+        #endregion
+
+        #region helper methods
+
+        private void CheckDistinctAppSettingKeys()
+        {
+            var appSettingKeys = this.AppSettingsElement.Elements("add").Select(e => e.Attribute("key").Value).ToList();
+
+            if (appSettingKeys.Count() != appSettingKeys.Distinct().Count())
+            {
+                const String Message = "The keys in the application file are not distinct";
+                this._logger.Fatal(Message);
+
+                throw new ApplicationException(Message);
+            }
+
         }
 
         #endregion
